866bd4480f5e91f7dbaf781594d688ab5ed35e99
riicchhaarrd <riicchhaarrd@users.noreply.github.com>
2021-05-04 12:11:25 +0200

    emacs config

---
+++ .emacs.d/init.el
@@ -0,0 +1,97 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(ccls yasnippet-snippets lsp-ui evil debian-el company-lsp)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(use-package lsp-mode :commands lsp)
+(use-package lsp-ui :commands lsp-ui-mode)
+(use-package company-lsp :commands company-lsp)
+
+(use-package ccls
+  :hook ((c-mode c++-mode objc-mode cuda-mode) .
+         (lambda () (require 'ccls) (lsp))))
+
+(setq ccls-executable "/usr/bin/ccls")
+;; (setq ccls-args '("--log-file=/tmp/ccls.log"))
+
+(global-linum-mode)
+(cua-mode 1)
+
+;; TODO
+;; Try out treemacs or neotree (treemacs looks nice)
+
+
+;; Set tabs indentation and width 4
+
+(setq-default indent-tabs-mode t)
+(setq-default tab-width 4) ; Assuming you want your tabs to be four spaces wide
+(defvaralias 'c-basic-offset 'tab-width)
+
+;; disable auto / smart indentation..
+
+(when (fboundp 'electric-indent-mode) (electric-indent-mode -1))
+
+;; tab and shift tab (backtab) line indenting
+;; copied from https://stackoverflow.com/questions/2249955/emacs-shift-tab-to-left-shift-the-block/2252922
+
+(defun indent-region-custom(numSpaces)
+    (progn 
+        ; default to start and end of current line
+        (setq regionStart (line-beginning-position))
+        (setq regionEnd (line-end-position))
+
+        ; if there's a selection, use that instead of the current line
+        (when (use-region-p)
+            (setq regionStart (region-beginning))
+            (setq regionEnd (region-end))
+        )
+
+        (save-excursion ; restore the position afterwards            
+            (goto-char regionStart) ; go to the start of region
+            (setq start (line-beginning-position)) ; save the start of the line
+            (goto-char regionEnd) ; go to the end of region
+            (setq end (line-end-position)) ; save the end of the line
+
+            (indent-rigidly start end numSpaces) ; indent between start and end
+            (setq deactivate-mark nil) ; restore the selected region
+        )
+    )
+)
+
+(defun untab-region (N)
+    (interactive "p")
+    (indent-region-custom -4)
+)
+
+(defun tab-region (N)
+    (interactive "p")
+    (if (active-minibuffer-window)
+        (minibuffer-complete)    ; tab is pressed in minibuffer window -> do completion
+    ; else
+    (if (string= (buffer-name) "*shell*")
+        (comint-dynamic-complete) ; in a shell, use tab completion
+    ; else
+    (if (use-region-p)    ; tab is pressed is any other buffer -> execute with space insertion
+        (indent-region-custom 4) ; region was selected, call indent-region
+        (insert "    ") ; else insert four spaces as expected
+    )))
+)
+
+(global-set-key (kbd "<backtab>") 'untab-region)
+(global-set-key (kbd "<tab>") 'tab-region)
+
