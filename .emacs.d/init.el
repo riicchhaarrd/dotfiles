ba3c918bd51cdda4218d65db671ab254d4f68bd7
Richard <riicchhaarrd@users.noreply.github.com>
2021-05-25 23:31:32 +0200

    lighter emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -70,10 +70,18 @@
 ;; set transparency
 (set-frame-parameter (selected-frame) 'alpha '(85 85))
 (add-to-list 'default-frame-alist '(alpha 85 85))
-
     
 ;; replace C-x b with ibuffer
 (global-set-key (kbd "C-x C-b") 'ibuffer)
+
+(with-eval-after-load 'evil-maps
+  (define-key evil-motion-state-map (kbd "SPC") nil)
+  (define-key evil-motion-state-map (kbd "RET") nil)
+  (define-key evil-insert-state-map (kbd "C-a") 'evil-beginning-of-line)
+  (define-key evil-normal-state-map (kbd "C-a") 'evil-beginning-of-line)
+  (define-key evil-insert-state-map (kbd "C-e") 'move-end-of-line)
+  (define-key evil-normal-state-map (kbd "C-e") 'move-end-of-line)
+  (define-key evil-motion-state-map (kbd "TAB") nil))
 
 ;; enable vi style keybinds
 (evil-mode 1)
@@ -108,13 +116,83 @@
 ;; requires undo-tree package
 ;; package-install undo-tree
 ;; https://emacs.stackexchange.com/questions/24496/integrate-cua-mode-with-evil-insert-state
+
+(defun copy-keep-highlight (beg end)
+  (interactive "r")
+  (prog1 (clipboard-kill-ring-save beg end)
+    (setq deactivate-mark nil)))
+
 (setq cua-keep-region-after-copy t)
-(define-key evil-insert-state-map (kbd "C-c") 'cua-copy-region)
-(define-key evil-insert-state-map (kbd "C-v") 'cua-paste)
+;; (define-key evil-insert-state-map (kbd "C-c") 'clipboard-kill-ring-save)
+(define-key evil-insert-state-map (kbd "C-c") 'copy-keep-highlight)
+(define-key evil-insert-state-map (kbd "C-v") 'clipboard-yank)
+;; (define-key evil-insert-state-map (kbd "C-a") 'mark-whole-buffer)
+;; (define-key evil-normal-state-map (kbd "C-a") 'mark-whole-buffer)
+;; (global-set-key (kbd "C-a") 'mark-whole-buffer)
+
 ;; (define-key evil-insert-state-map (kbd "C-x") 'cua-cut-region)
 (define-key evil-insert-state-map (kbd "C-z") 'undo-tree-undo)
 (define-key evil-insert-state-map (kbd "C-y") 'undo-tree-redo)
 (define-key evil-insert-state-map (kbd "C-d") 'duplicate-line)
+
+(define-key evil-normal-state-map (kbd "s-=") 'evil-numbers/inc-at-pt)
+(define-key evil-visual-state-map (kbd "s-=") 'evil-numbers/inc-at-pt)
+(define-key evil-normal-state-map (kbd "s--") 'evil-numbers/dec-at-pt)
+(define-key evil-visual-state-map (kbd "s--") 'evil-numbers/dec-at-pt)
+
+;; note to self
+;; ctrl + o and ctrl + i for jump list (evil-mode)
+;; and ctrl + ] to jump to definition
+
+;; ctrl-z toggle evil-mode and emacs mode
+;; ctrl + h + k, find out what key does what
+
+;; uncommenting and commenting lines is Shift +V + M-;
+
+;; this interferes with quickly typing any keys that require shift and just pressing space e.g +
+;; (global-set-key (kbd "S-SPC") 'evil-normal-state)
+
+;; vi style keybindings in evil mode
+
+(defun kbd+ (keyrep &optional need-vector)
+  (if (vectorp keyrep) keyrep (edmacro-parse-keys keyrep need-vector)))
+
+(defun gmap (keyrep defstr)
+  "Vim-style global keybinding. Uses the `global-set-key' binding function."
+  (global-set-key (kbd+ keyrep) (edmacro-parse-keys defstr t)))
+
+(defun fmap (keybind-fn keyrep defstr)
+  "Vim-style keybinding using the key binding function KEYBIND-FN."
+  (call keybind-fn (kbd+ keyrep) (edmacro-parse-keys defstr t)))
+
+(defun xmap (keymap keyrep defstr)
+  "Vim-style keybinding in KEYMAP. Uses the `define-key' binding function."
+  (define-key keymap (kbd+ keyrep) (edmacro-parse-keys defstr t)))
+
+(defun nmap (keyrep defstr) "Vim-style keybinding for `evil-normal-state'. Uses the `define-key' binding function."
+      (xmap evil-normal-state-map keyrep defstr))
+(defun imap (keyrep defstr) "Vim-style keybinding for `evil-insert-state'. Uses the `define-key' binding function."
+      (xmap evil-insert-state-map keyrep defstr))
+(defun vmap (keyrep defstr) "Vim-style keybinding for `evil-visual-state'. Uses the `define-key' binding function."
+      (xmap evil-visual-state-map keyrep defstr))
+(defun mmap (keyrep defstr) "Vim-style keybinding for `evil-motion-state'. Uses the `define-key' binding function."
+      (xmap evil-motion-state-map keyrep defstr))
+
+(defun test2()
+  (interactive)
+  (move-beginning-of-line 1)
+  (open-line 1)
+  (next-line 1)
+)
+(defun test3()
+  (interactive)
+  (move-beginning-of-line 1)
+  (previous-line 1)
+  (kill-line)
+)
+(define-key evil-normal-state-map (kbd "RET") 'test2)
+(define-key evil-normal-state-map (kbd "DEL") 'test3)
+
 ;; (global-set-key (kbd "C-d") 'duplicate-line)
 
 ;; show which keycombos and their commands
@@ -126,25 +204,11 @@
 ;; set visual select color
 (set-face-attribute 'region nil :background "#666")
 
-;; enable c autocomplete stuff
-(use-package lsp-mode :commands lsp)
-(use-package lsp-ui :commands lsp-ui-mode)
-(use-package company-lsp :commands company-lsp)
-
-(use-package ccls
-  :hook ((c-mode c++-mode objc-mode cuda-mode) .
-         (lambda () (require 'ccls) (lsp))))
-
-(setq lsp-file-watch-threshold 2000)
-
-(setq ccls-executable "/usr/bin/ccls")
-;; (setq ccls-args '("--log-file=/tmp/ccls.log"))
-
 ;; save/restore opened files and windows config
-(desktop-save-mode 1) ; 0 for off
+;; (desktop-save-mode 1) ; 0 for off
 
 ;; load custom theme
-(load-theme 'leuven t)
+;; (load-theme 'leuven t)
 
 ;; disable auto / smart indentation..
 ;; (when (fboundp 'electric-indent-mode) (electric-indent-mode -1))
@@ -200,104 +264,6 @@
 (global-set-key (kbd "<backtab>") 'untab-region)
 (global-set-key (kbd "<tab>") 'tab-region)
 
-;; use tab-bar-mode to have tabs at the top
-;; (tab-bar-mode)
-;; using centaur-tabs for now
-
-(use-package centaur-tabs
-  :demand
-  :config
-  (centaur-tabs-mode t)
-  :bind
-  ("C-c [" . centaur-tabs-backward)
-  ("C-c ]" . centaur-tabs-forward))
-
-(centaur-tabs-headline-match)
-(setq centaur-tabs-style "wave")
-(setq centaur-tabs-set-bar 'left)
-(setq centaur-tabs-set-bar 'under)
-;; Note: If you're not using Spacmeacs, in order for the underline to display
-;; correctly you must add the following line:
-(setq x-underline-at-descent-line t)
-(setq centaur-tabs-set-modified-marker t)
-(setq centaur-tabs-modified-marker "*")
-(centaur-tabs-change-fonts "Open Sans" 80)
-
-(defun centaur-tabs-hide-tab (x)
-  "Do no to show buffer X in tabs."
-  (let ((name (format "%s" x)))
-    (or
-     ;; Current window is not dedicated window.
-     (window-dedicated-p (selected-window))
-
-     ;; Buffer name not match below blacklist.
-     (string-prefix-p "*epc" name)
-     (string-prefix-p "*helm" name)
-     (string-prefix-p "*Helm" name)
-     (string-prefix-p "*Compile-Log*" name)
-     (string-prefix-p "*lsp" name)
-     (string-prefix-p "*company" name)
-     (string-prefix-p "*Flycheck" name)
-     (string-prefix-p "*tramp" name)
-     (string-prefix-p " *Mini" name)
-     (string-prefix-p "*help" name)
-     (string-prefix-p "*straight" name)
-     (string-prefix-p " *temp" name)
-     (string-prefix-p "*Help" name)
-     (string-prefix-p "*mybuf" name)
-
-     ;; Is not magit buffer.
-     (and (string-prefix-p "magit" name)
-	  (not (file-name-extension name)))
-     )))
-
-;; custom variables set by emacs self
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("3957edd5051733cf0f50c184f3ef41fa181706fbc7ac2043d1f3b4d0034b2fe3" "f08d2081f6783a5712cdce418f3962bd97a2054e8960609aad53f013a8b6f1cc" default))
- '(package-selected-packages
-   '(clang-format centaur-tabs dap-mode logview neotree treemacs-evil treemacs vscode-dark-plus-theme vs-light-theme expand-region yasnippet-snippets lsp-ui evil company-lsp which-key use-package ccls ac-php)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Consolas for Powerline" :foundry "MS  " :slant normal :weight normal :height 82 :width normal)))))
-
-;; bind CTRL-C t to toggle treemacs
-(global-set-key (kbd "C-c t") 'treemacs)
-
-;; (set-face-attribute 'treemacs-directory-face nil :family "Consolas for Powerline")
-(use-package treemacs)
-(dolist (face '(treemacs-root-face
-              treemacs-git-unmodified-face
-              treemacs-git-modified-face
-              treemacs-git-renamed-face
-              treemacs-git-ignored-face
-              treemacs-git-untracked-face
-              treemacs-git-added-face
-              treemacs-git-conflict-face
-              treemacs-directory-face
-              treemacs-directory-collapsed-face
-              treemacs-file-face
-              treemacs-tags-face))
-(set-face-attribute face nil :family "Open Sans" :height 80))
-
-(projectile-mode +1)
-(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
-(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
-
-;; enable server
-;; (server-start)
-(use-package dap-mode)
-(dap-mode 1)
-(dap-ui-mode 1)
-(require 'dap-cpptools)
 (setq inhibit-eol-conversion t)
 
 ;; disables some parentheses mode autocomplete
@@ -328,3 +294,48 @@
 
 ;; To be able to select identifiers with underscores easier
 (superword-mode)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-quickhelp-color-background "#4F4F4F")
+ '(company-quickhelp-color-foreground "#DCDCCC")
+ '(custom-enabled-themes '(leuven))
+ '(custom-safe-themes
+   '("e6df46d5085fde0ad56a46ef69ebb388193080cc9819e2d6024c9c6e27388ba9" default))
+ '(fci-rule-color "#383838")
+ '(nrepl-message-colors
+   '("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3"))
+ '(package-selected-packages
+   '(evil-numbers zenburn-theme yasnippet-snippets lsp-ui evil company-lsp which-key expand-region))
+ '(pdf-view-midnight-colors '("#DCDCCC" . "#383838"))
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map
+   '((20 . "#BC8383")
+	 (40 . "#CC9393")
+	 (60 . "#DFAF8F")
+	 (80 . "#D0BF8F")
+	 (100 . "#E0CF9F")
+	 (120 . "#F0DFAF")
+	 (140 . "#5F7F5F")
+	 (160 . "#7F9F7F")
+	 (180 . "#8FB28F")
+	 (200 . "#9FC59F")
+	 (220 . "#AFD8AF")
+	 (240 . "#BFEBBF")
+	 (260 . "#93E0E3")
+	 (280 . "#6CA0A3")
+	 (300 . "#7CB8BB")
+	 (320 . "#8CD0D3")
+	 (340 . "#94BFF3")
+	 (360 . "#DC8CC3")))
+ '(vc-annotate-very-old-color "#DC8CC3"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Noto Sans Mono" :foundry "GOOG" :slant normal :weight normal :height 119 :width normal)))))
+
+(define-key global-map [?\s-p] 'goto-line)
